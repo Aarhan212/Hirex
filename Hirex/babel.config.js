5b0639e826406df32f25e8b743cef8b321cd2eb8
Aarhan Khan <aarhankhan212@gmail.com>
2021-10-15 13:08:16 +0530

    bug fixes

--- Hirex/babel.config.js
+++ Hirex/babel.config.js
@@ -2,13 +2,8 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-<<<<<<< HEAD
-  };
-};
-=======
     plugins: [
       "module:react-native-dotenv",
     ]
   };
 };
->>>>>>> parent of ddb4ce4 (delete)
